3e6e4f366b493645d04e965169bb877792bd449d
Jeremy Glize <le-schlouze@hotmail.fr>
2020-08-12 08:54:37 +0200

    article wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Clean DB"
+Article.destroy_all
+puts "Create article"
+Article.create! (
+  title: Faker::Space.constellation
+  content: Faker::Space.galaxy)
+puts "DB Ok"
+
